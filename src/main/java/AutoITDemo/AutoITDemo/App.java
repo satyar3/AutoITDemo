22ba817f0ead9b6cfa51d5b3b37b3be76fe34c7c
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2019-07-27 12:23:37 +0530

    AutoIT Updated

--- src/main/java/AutoITDemo/AutoITDemo/App.java
+++ src/main/java/AutoITDemo/AutoITDemo/App.java
@@ -14,11 +14,13 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+    	String filePath = "C:\\Back Up\\P Workspace\\AutoITDemo\\Resources\\FIle.txt";
         System.setProperty("webdriver.chrome.driver", "C:\\Back Up\\P Workspace\\AutoITDemo\\Resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("file:///C:/Back%20Up/P%20Workspace/AutoITDemo/Resources/Browse.html");
         driver.findElement(By.id("1")).click();
         
         Runtime.getRuntime().exec("C:\\Back Up\\P Workspace\\AutoITDemo\\Resources\\FileUpload.exe");
+        Runtime.getRuntime().exec("C:\\Back Up\\P Workspace\\AutoITDemo\\Resources\\AnyBrowser.exe" + " "+ filePath);
     }
 }
